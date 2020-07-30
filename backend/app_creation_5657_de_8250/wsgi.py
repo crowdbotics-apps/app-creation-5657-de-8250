1ab369f8ce362242e18b398a9445254e3124cd8a
Crowdbotics <team@crowdbotics.com>
2020-07-30 19:18:14 +0000

    Update models

--- backend/app_creation_5657_de_8250/wsgi.py
+++ backend/app_creation_5657_de_8250/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_creation_5657_de_8250.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_creation_5657_de_8250.settings")
 
 application = get_wsgi_application()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,44 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
+    sasfsdf = models.OneToOneField(
+        "home.Test",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="customtext_sasfsdf",
+    )
+    dfsasa = models.ForeignKey(
+        "users.User",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="customtext_dfsasa",
+    )
+    sadfasf = models.ManyToManyField(
+        "home.Fnhgjhjgjh", blank=True, related_name="customtext_sadfasf",
+    )
+    sdfdsf = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="customtext_sdfdsf",
+    )
+    fdgg = models.BigIntegerField(null=True, blank=True,)
+    sdfsdf = models.BigIntegerField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +52,18 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class Fnhgjhjgjh(models.Model):
+    "Generated Model"
+    jhgkhgkjhg = models.BigIntegerField()
+
+
+class Test(models.Model):
+    "Generated Model"
+    nkhgkhg = models.BigIntegerField()
